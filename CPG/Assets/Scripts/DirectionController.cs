82a1a17520986f1f00fbb246572fbfbd61c5f8e0
Felipe Conrado <fconrado2003@gmail.com>
2024-05-18 13:13:52 -0300

    FUNCIONA HAHAAHAHHA

--- CPG/Assets/Scripts/DirectionController.cs
+++ CPG/Assets/Scripts/DirectionController.cs
@@ -6,22 +6,24 @@ public class DirectionController : MonoBehaviour
 {
     private Transform player;
     public float dist = 2;
+    public float speed = 5;
+    private Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponentInParent<PlayerController>().gameObject.transform;
+        offset = transform.position - player.position;
+        offset = offset.normalized * dist;
     }
 
     // Update is called once per fram
     void Update()
     {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.z = 0; 
-        Vector3 direction = mousePosition - player.position;
-        direction = Vector3.ClampMagnitude(direction, dist); 
-        direction = direction.normalized * dist;     
-        Vector3 clampedMousePosition = player.position + direction;
+        float dir = Input.GetAxis("Horizontal");
+        float angle = speed * dir;
+        Debug.Log(angle);
+        offset = Quaternion.Euler(0, 0, angle) * offset;
 
-        transform.position = clampedMousePosition;
+        transform.position = player.position + offset;
     }
 }
